b1adebc62c4a5342addff8f83fa63b80961cbb19
jinhuaiyao <20178218+jinhuaiyao@users.noreply.github.com>
2022-01-30 07:43:44 +0800

    part-ix-pagination-1

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,3 +1,4 @@
+from typing import Text
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
@@ -52,3 +53,9 @@ class EditTIDForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired(),Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+    
