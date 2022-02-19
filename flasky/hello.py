8b9bbe65aa83893a13741d85305ace5e8e740615
jinhuaiyao <20178218+jinhuaiyao@users.noreply.github.com>
2022-02-19 21:31:31 +0800

    done P41

--- flasky/hello.py
+++ flasky/hello.py
@@ -3,9 +3,22 @@ from ensurepip import bootstrap
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/') 
 def index():
@@ -16,9 +29,9 @@ def index():
     #c = request.remote_addr
     #out = str(a) + "\n" + str(b) + "\n" + str(c)
     #return out
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 
 @app.route('/user/<name>') 
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html',name=name, current_time=datetime.utcnow())
 
