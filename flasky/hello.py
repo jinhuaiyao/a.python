fc29e235654106de8f5ce07bae010bd52033c47a
jinhuaiyao <20178218+jinhuaiyao@users.noreply.github.com>
2022-02-21 20:01:52 +0800

    page 51

--- flasky/hello.py
+++ flasky/hello.py
@@ -6,7 +6,14 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -20,7 +27,11 @@ def page_not_found(e):
     return render_template('500.html'), 500
 
 
-@app.route('/') 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST']) 
 def index():
     #return '<h1>Hello World!</h1>'
     #user_agent = request.headers.get('User-Agent') 
@@ -29,7 +40,12 @@ def index():
     #c = request.remote_addr
     #out = str(a) + "\n" + str(b) + "\n" + str(c)
     #return out
-    return render_template('index.html',current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+    
+    return render_template('index.html',current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>') 
 def user(name):
