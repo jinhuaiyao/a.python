0a2c18013d1430fb9e6bf65ec64fd4624c7bcf75
jinhuaiyao <20178218+jinhuaiyao@users.noreply.github.com>
2022-02-22 16:49:46 +0800

    page 71

--- flasky/hello.py
+++ flasky/hello.py
@@ -30,7 +30,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -45,7 +45,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-        
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -71,13 +71,26 @@ def index():
     #return out
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+
+        print('aaa ' + str(session.get('known1', False)))
+        print('bbb ' + str(session.get('known1', True)))
+        print('ccc ' + str(session.get('known1', None)))
+        print('ddd ' + str(session.get('known1')))
+
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     
-    return render_template('index.html',current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html',current_time=datetime.utcnow(), 
+        form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>') 
 def user(name):
